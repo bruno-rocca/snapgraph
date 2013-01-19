319c27dce325c1c197f571bb52469a038c93cf05
Jasdev Singh <jasdevs@gmail.com>
2013-01-18 23:26:12 -0500

    progress on getuser

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require('express');
-var nodeio = require('node.io');
+var scraper = require('./routes.js');
 var app = express.createServer(express.logger());
 
-app.get('/', function(req, res) {
-  res.send('index');
+app.get('/getuser', function(req, res) {
+  var name = req.query.u;
+  
+  if (typeof name === "undefined"){
+	res.send({"error":"No username given"});
+  }
+  else{
+	console.log("Making a request for user: " + name);
+	var result = scraper.getUser(name);
+	res.send(result);
+  }
 });
 
 var port = process.env.PORT || 5000;
