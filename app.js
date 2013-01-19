01fbeeadfa6fd693e83611560613cf043108a028
Jasdev Singh <jasdevs@gmail.com>
2013-01-18 21:42:01 -0500

    fix express issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.send('index');
 });
 
 var port = process.env.PORT || 5000;
