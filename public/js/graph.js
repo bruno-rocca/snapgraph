ad6eaeda31364f8c16343061bc2a93e4935e2cb2
David Hu <octopie@gmail.com>
2013-01-19 23:44:05 -0500

    refactoring JS and disabling input on search

--- public/js/graph.js
+++ public/js/graph.js
@@ -1,117 +1,164 @@
-var w = 960,
-    h = 500,
-    node,
-    link,
-    root,
-    colorscale = d3.scale.category10();
+function myGraph(el) {
+    // Add and remove elements on the graph object
+    this.addNode = function (id) {
+        nodes.push({"id":id, "attraction": {} });
+        update();
+    }
 
-var force = d3.layout.force()
-    .on("tick", tick)
-    .charge(function(d) { return d._children ? -d.size : -30; })
-    .linkDistance(function(d) { return d.target._children ? 80 : 30; })
-    .size([w, h]);
+    this.removeNode = function (id) {
+        var i = 0;
+        var n = findNode(id);
+        while (i < links.length) {
+            if ((links[i]['source'] == n)||(links[i]['target'] == n)) links.splice(i,1);
+            else i++;
+        }
+        nodes.splice(findNodeIndex(id),1);
+        update();
+    }
 
-var vis = d3.select("#chart").append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    this.addLink = function (source, target, strength) {
+        links.push({"source":findNode(source),"target":findNode(target)});
+        findNode(source).attraction[target] = strength*2.4;
+        update();
+    }
 
-d3.json("sample.json", function(json) {
-  root = json;
-  root.fixed = true;
-  root.x = w / 2;
-  root.y = h / 2;
-  update();
-});
+    var findNode = function(id) {
+        for (var i in nodes) {if (nodes[i]["id"] === id) return nodes[i]};
+    }
 
-function update() {
-  var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
+    var findNodeIndex = function(id) {
+        for (var i in nodes) {if (nodes[i]["id"] === id) return i};
+    }
 
-  // Restart the force layout.
-  force
-      .nodes(nodes)
-      .links(links)
-      .start();
+    // set up the D3 visualisation in the specified element
+    var w = $(el).innerWidth(),
+        h = $(el).innerHeight();
 
-  // Update the links…
-  link = vis.selectAll("line.link")
-      .data(links, function(d) { return d.target.id; });
+    var vis = this.vis = d3.select(el).append("svg:svg")
+        .attr("width", w)
+        .attr("height", h);
 
-  // Enter any new links.
-  link.enter().insert("line", ".node")
-      .attr("class", "link")
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+    var force = d3.layout.force()
+        .gravity(.05)
+        .distance(function(d) {
+            // calculate distance based on sum of their attractions
+            var dist = 120 - attractionSum(d);
+            return dist > 40 ? dist : 40; })
+        .charge(-500)
+        .size([w, h]);
 
-  // Exit any old links.
-  link.exit().remove();
+    var nodes = force.nodes(),
+        links = force.links();
 
-  // Update the nodes…
-  node = vis.selectAll("circle.node")
-      .data(nodes, function(d) { return d.id; })
-      .style("fill", color);
+    var update = function () {
 
-  node.transition()
-      .attr("r", function(d) { return d.children ? 4.5 : d.size; });
+        var link = vis.selectAll("line.link")
+            .data(links, function(d) { return d.source.id + "-" + d.target.id; });
 
-  // Enter any new nodes.
-  node.enter().append("circle")
-      .attr("class", "node")
-      .attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; })
-      .attr("r", function(d) { return d.children ? 4.5 : d.size; })
-      .style("fill", color)
-      .on("click", click)
-      .call(force.drag);
+        link.enter().insert("line")
+            .attr("class", "link")
+            .style("stroke", function(d) {
+                var sum = attractionSum(d);
+                return d3.rgb(158+2*sum, 202-sum, 225-2*sum);
+            })
+            .style("stroke-width", function(d) {
+                var sum = attractionSum(d);
+                return 2 + (sum/20);
+            });
 
-  node.append("title")
-      .text(function(d) { return d.name; });
+        link.exit().remove();
 
-  // Exit any old nodes.
-  node.exit().remove();
-}
+        var node = vis.selectAll("g.node")
+            .data(nodes, function(d) { return d.id;});
 
-function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+        var nodeEnter = node.enter().append("g")
+            .attr("class", "node")
+            .on("mouseover", mouseover)
+            .on("mouseout", mouseout)
+            .call(force.drag);
 
-  node.attr("cx", function(d) { return d.x; })
-      .attr("cy", function(d) { return d.y; });
-}
 
-// Color leaf nodes orange, and packages white or blue.
-function color(d) {
-//  return d._children ? "#3182bd" : d.children ?  : "#fd8d3c";
-  return d._children ? "#3182bd" : d.children ? "#c6dbef" : colorscale(d.size);
-}
+        node.append("circle")
+            // .attr("cx", function(d) { return d.x + Math.random()*100; })
+            // .attr("cy", function(d) { return d.y; })
+            .attr("r", 30)
+            .style("fill", "#930");
 
-// Toggle children on click.
-function click(d) {
-  if (d.children) {
-    d._children = d.children;
-    d.children = null;
-  } else {
-    d.children = d._children;
-    d._children = null;
-  }
-  update();
-}
+        node.append("svg:image")
+            .attr('xlink:href', '../img/snapchat.jpg')
+            .attr("width", 36)
+            .attr("height", 36)
+            .attr("x", -18)
+            .attr("y", -18)
+            .call(fetchTwitterImg);
 
-// Returns a list of all nodes under the root.
-function flatten(root) {
-  var nodes = [], i = 0;
 
-  function recurse(node) {
-    if (node.children) node.size = node.children.reduce(function(p, v) { return p + recurse(v); }, 0);
-    if (!node.id) node.id = ++i;
-    nodes.push(node);
-    return node.size;
-  }
+        node.exit().remove();
 
-  root.size = recurse(root);
-  return nodes;
+        force.on("tick", function() {
+            
+            node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+        });
+
+        node.order();
+
+        // Restart the force layout.
+        force.start();
+
+
+    } // end update
+
+    function attractionSum(d) {
+        var sum = 0;
+        if(d.source.attraction[d.target.id] !== undefined)
+            sum += d.source.attraction[d.target.id];
+        if(d.target.attraction[d.source.id] !== undefined)
+            sum += d.target.attraction[d.source.id];
+        return sum;
+    }
+
+    function fetchTwitterImg(selection) {
+        var users = selection[0];
+        for(var i = 0; i < users.length; i++) {
+            // if always runs, that means we got an image back so we the node's image 
+            var currUser = users[i];
+            if(currUser !== null) {
+                var username = users[i].__data__.id;
+                console.log('username is', username);   
+                $.ajax({
+                    url:'https://api.twitter.com/1/users/profile_image?screen_name='+username+'&size=bigger',
+                    crossDomain: true,
+                    dataType: 'jsonp'
+                }).always(function(eye, usrnm) { 
+                    return function(a, b) {
+                        users[eye].href.baseVal = 'https://api.twitter.com/1/users/profile_image?screen_name='+usrnm+'&size=bigger';
+                    }
+                }(i, username));
+            }
+        }
+    }
+
+    // show tooltip
+    function mouseover(d) {
+        console.log(d3.select(d));
+        var svg = d3.select(d)[0][0];
+        if(!svg.id) {
+            $('#tooltip').html(svg._id);
+        } else {
+            $('#tooltip').html(svg.id); 
+        }
+        $('#tooltip').show(0);
+    }
+
+    // hide tooltip
+    function mouseout(d) {
+        $('#tooltip').hide(0);
+    }
+
+    // Make it all go
+    update();
 }
